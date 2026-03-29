27c8241bb02d8d4e2d3c56c19368926067d0a1ab
Ivan Paraskun <ig.paraskun@gmail.com>
2026-03-29 22:44:03 +0700

    dev: add cmake

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 touch $HOME/.bash_profile
+
 conda init
 conda env create -f .devcontainer/environment.yaml
-
 echo "conda activate v2k" >> $HOME/.bash_profile
 
+go install github.com/magefile/mage@latest
