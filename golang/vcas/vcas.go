e27224590fad2cb0e5c0de9aac1b0125b98c40b0
Ivan Paraskun <ig.paraskun@gmail.com>
2026-04-02 12:46:18 +0700

    vcas: externalize as golang library

---
+++ golang/vcas/vcas.go
@@ -0,0 +1,196 @@
+package vcas
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+const (
+	Timestamp = "02.01.2006 15_04_05.000"
+
+	NONE Method = iota + 1
+	SET
+	GET
+	GETFULL
+	SUB
+	UNSUB
+	CREATE
+	FREE
+
+	OuterSep = '|'
+	InnerSep = ':'
+)
+
+type Method int
+
+func (m *Method) unmarshal(s string) error {
+	switch s {
+	case "s", "set":
+		*m = SET
+	case "g", "get":
+		*m = GET
+	case "gf", "getfull":
+		*m = GETFULL
+	case "sb", "subscr", "subscribe":
+		*m = SUB
+	case "rel", "release":
+		*m = UNSUB
+	case "cr", "create":
+		*m = CREATE
+	case "f", "free":
+		*m = FREE
+	default:
+		return fmt.Errorf("unknown: %v", s)
+	}
+
+	return nil
+}
+
+func (m Method) marshal() string {
+	switch m {
+	case SET:
+		return "set"
+	case GET:
+		return "get"
+	case GETFULL:
+		return "getfull"
+	case SUB:
+		return "subscribe"
+	case UNSUB:
+		return "release"
+	case CREATE:
+		return "create"
+	case FREE:
+		return "free"
+	default:
+		return ""
+	}
+}
+
+type Packet struct {
+	Time   time.Time
+	Topic  string
+	Method Method
+	Value  any
+}
+
+func Marshal(pkt *Packet, pay []byte, extra map[string]string) ([]byte, error) {
+	if pkt.Topic == "" {
+		return nil, fmt.Errorf("name is missing, but required")
+	}
+
+	val := "none"
+
+	if pkt.Value != nil {
+		switch v := pkt.Value.(type) {
+		case float64:
+			val = strconv.FormatFloat(v, 'f', -1, 64)
+		case int64:
+			val = strconv.FormatInt(v, 10)
+		case string:
+			val = v
+		default:
+			return nil, fmt.Errorf("unsupported value type")
+		}
+	}
+
+	buf := bytes.NewBuffer(pay)
+	size := 40 + len(pkt.Topic) + len(val)
+	method := pkt.Method.marshal()
+
+	if pkt.Method != NONE {
+		size += 8 + len(method)
+	}
+
+	buf.Grow(size)
+	buf.WriteString("time:")
+	buf.WriteString(pkt.Time.Local().Format(Timestamp))
+
+	if pkt.Method != NONE {
+		buf.WriteString("|method:")
+		buf.WriteString(method)
+	}
+
+	buf.WriteString("|name:")
+	buf.WriteString(pkt.Topic)
+	buf.WriteString("|val:")
+	buf.WriteString(val)
+
+	for k, v := range extra {
+		buf.WriteRune('|')
+		buf.WriteString(k)
+		buf.WriteRune(':')
+		buf.WriteString(v)
+	}
+
+	buf.WriteRune('\n')
+
+	return buf.Bytes(), nil
+}
+
+func Unmarshal(pkt *Packet, pay []byte, extra map[string]string) error {
+	pkt.Topic = ""
+	pkt.Method = NONE
+	pkt.Time = time.Now()
+	pkt.Value = nil
+
+	for tok := range bytes.SplitSeq(pay, []byte{OuterSep}) {
+		tok := bytes.SplitN(tok, []byte{InnerSep}, 2)
+
+		if len(tok) != 2 {
+			continue
+		}
+
+		k := string(bytes.Trim(tok[0], "\n\t\r "))
+		v := string(tok[1])
+
+		switch k {
+		case "method", "meth", "m":
+			if err := pkt.Method.unmarshal(v); err != nil {
+				return fmt.Errorf("method: %w", err)
+			}
+		case "time", "t":
+			time, err := time.ParseInLocation(Timestamp, v, time.Local)
+
+			if err != nil {
+				return fmt.Errorf("time: %v", err)
+			}
+
+			pkt.Time = time
+		case "name", "n":
+			pkt.Topic = v
+		case "value", "val", "v":
+			if v == "none" || v == "" {
+				break
+			}
+
+			i64, err := strconv.ParseInt(v, 10, 64)
+
+			if err == nil {
+				pkt.Value = i64
+				break
+			}
+
+			f64, err := strconv.ParseFloat(v, 64)
+
+			if err == nil {
+				pkt.Value = f64
+				break
+			}
+
+			pkt.Value = v
+		default:
+			if extra != nil {
+				extra[k] = v
+			}
+		}
+	}
+
+	if pkt.Topic == "" {
+		return fmt.Errorf("name is missing, but required")
+	}
+
+	return nil
+}
---
+++ golang/vcas/vcas_test.go
@@ -0,0 +1,541 @@
+package vcas
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	fmt.Println("Local:", time.Now().Format(time.RFC1123))
+	fmt.Println("Location Name:", time.Local.String())
+	fmt.Println("Is DST:", time.Now().IsDST())
+}
+
+func TestMarshal(t *testing.T) {
+	cases := map[string]struct {
+		inp Packet
+		exp struct {
+			err bool
+			res string
+		}
+	}{
+		`with float`: {
+			inp: Packet{
+				Method: SET,
+				Topic:  "test",
+				Time:   time.UnixMilli(1118505599999),
+				Value:  11.06,
+			},
+			exp: struct {
+				err bool
+				res string
+			}{
+				err: false,
+				res: "time:11.06.2005 23_59_59.999|method:set|name:test|val:11.06\n",
+			},
+		},
+		`with string`: {
+			inp: Packet{
+				Method: SET,
+				Topic:  "test",
+				Time:   time.UnixMilli(1118505599999),
+				Value:  "hello",
+			},
+			exp: struct {
+				err bool
+				res string
+			}{
+				err: false,
+				res: "time:11.06.2005 23_59_59.999|method:set|name:test|val:hello\n",
+			},
+		},
+		`without value`: {
+			inp: Packet{
+				Method: SET,
+				Topic:  "test",
+				Time:   time.UnixMilli(1118505599999),
+			},
+			exp: struct {
+				err bool
+				res string
+			}{
+				err: false,
+				res: "time:11.06.2005 23_59_59.999|method:set|name:test|val:none\n",
+			},
+		},
+		`without name`: {
+			inp: Packet{
+				Method: SET,
+				Time:   time.UnixMilli(1118505599999),
+			},
+			exp: struct {
+				err bool
+				res string
+			}{
+				err: true,
+			},
+		},
+	}
+
+	for name, data := range cases {
+		t.Run(name, func(t *testing.T) {
+			res := make([]byte, 0)
+			res, err := Marshal(&data.inp, res, nil)
+
+			if !data.exp.err {
+				assert.Nil(t, err)
+				assert.Equal(t, data.exp.res, string(res))
+			} else {
+				assert.NotNil(t, err)
+			}
+		})
+	}
+}
+
+func BenchmarkMarshal(b *testing.B) {
+	for b.Loop() {
+		b.StopTimer()
+
+		pay := make([]byte, 0)
+		pkt := Packet{
+			Method: SET,
+			Topic:  "VEPP/CCD/1M1L/sigma_x",
+			Time:   time.UnixMilli(1118505599999).Local(),
+			Value:  rand.Float64(),
+		}
+
+		b.StartTimer()
+		Marshal(&pkt, pay, nil)
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	cases := map[string]struct {
+		inp string
+		exp struct {
+			err bool
+			res Packet
+		}
+	}{
+		`publish (set)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:set|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999).Local(),
+					Value:  11.06,
+				},
+			},
+		},
+		`publish (s)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:s|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999).Local(),
+					Value:  11.06,
+				},
+			},
+		},
+		`subscribe`: {
+			inp: "time:11.06.2005 23_59_59.999|method:subscribe|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SUB,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999).Local(),
+					Value:  nil,
+				},
+			},
+		},
+		`subscribe (subscr)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:subscr|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SUB,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999).Local(),
+					Value:  nil,
+				},
+			},
+		},
+		`subscribe (sb)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:sb|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SUB,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999).Local(),
+					Value:  nil,
+				},
+			},
+		},
+		`unsubscribe (release)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:release|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: UNSUB,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999).Local(),
+					Value:  nil,
+				},
+			},
+		},
+		`unsubscribe (rel)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:rel|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: UNSUB,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  nil,
+				},
+			},
+		},
+		`get (getfull)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:getfull|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: GET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  nil,
+				},
+			},
+		},
+		`get (get)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:get|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: GET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  nil,
+				},
+			},
+		},
+		`get (gf)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:gf|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: GET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  nil,
+				},
+			},
+		},
+		`get (g)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:g|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: GET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  nil,
+				},
+			},
+		},
+		`time (t)`: {
+			inp: "t:11.06.2005 23_59_59.999|method:set|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`method (meth)`: {
+			inp: "time:11.06.2005 23_59_59.999|meth:set|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`method (m)`: {
+			inp: "time:11.06.2005 23_59_59.999|m:set|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`name (n)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:set|n:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`value (val)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:set|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`value (v)`: {
+			inp: "time:11.06.2005 23_59_59.999|method:set|name:test|v:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`with leading escapes`: {
+			inp: "\t\t  time:11.06.2005 23_59_59.999|method:set|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`with trailing escapes`: {
+			inp: "time:11.06.2005 23_59_59.999|method:set|name:test|val:11.06\t\t ",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  "11.06\t\t ",
+				},
+			},
+		},
+		`with inner escapes`: {
+			inp: "time:11.06.2005 23_59_59.999|\t\t method:set|name :test| val\t:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`with extra token`: {
+			inp: "time:11.06.2005 23_59_59.999|extra:none|method:set|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`with empty value`: {
+			inp: "time:11.06.2005 23_59_59.999|method:set|name:test|val:|descr:none|type:rw|units:none",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  nil,
+				},
+			},
+		},
+		`with none value`: {
+			inp: "time:11.06.2005 23_59_59.999|method:set|name:test|val:none",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: SET,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  nil,
+				},
+			},
+		},
+		`with malformed time`: {
+			inp: "time:11.06.2005 23:59:59.999|method:set|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: true,
+			},
+		},
+		`without name`: {
+			inp: "time:11.06.2005 23_59_59.999|method:set|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: true,
+			},
+		},
+		`without method`: {
+			inp: "time:11.06.2005 23_59_59.999|name:test|val:11.06",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: false,
+				res: Packet{
+					Method: NONE,
+					Topic:  "test",
+					Time:   time.UnixMilli(1118505599999),
+					Value:  11.06,
+				},
+			},
+		},
+		`with unknown method`: {
+			inp: "time:11.06.2005 23_59_59.999|method:extra|name:test",
+			exp: struct {
+				err bool
+				res Packet
+			}{
+				err: true,
+			},
+		},
+	}
+
+	for name, data := range cases {
+		t.Run(name, func(t *testing.T) {
+			pkt := Packet{}
+			err := Unmarshal(&pkt, []byte(data.inp), nil)
+
+			if !data.exp.err {
+				assert.Nil(t, err)
+				assert.Equal(t, data.exp.res, pkt)
+			} else {
+				assert.NotNil(t, err)
+			}
+		})
+	}
+}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	for b.Loop() {
+		b.StopTimer()
+
+		pkt := Packet{}
+		pay := fmt.Appendf(nil,
+			"time:11.06.2005 23_59_59.999|method:subscribe|name:VEPP/CCD/1M1L/sigma_x|val:%f",
+			rand.Float64(),
+		)
+
+		b.StartTimer()
+		Unmarshal(&pkt, pay, nil)
+	}
+}
