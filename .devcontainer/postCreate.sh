dee53b1bb6c511b949e83f691e2bda577fbcbcc0
Ivan Paraskun <ig.paraskun@gmail.com>
2026-04-14 12:00:32 +0700

    dev: add rpk feature

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-conda init && conda install -c conda-forge -y \
-  mkdocs                                      \
-  mkdocs-material                             \
-  mkdocs-monorepo-plugin
-
-go install github.com/magefile/mage@latest
+conda init
+task rpk:profile:update
 
 cd /workspaces/v2k/native/extern/vcpkg && . bootstrap-vcpkg.sh && cd /workspaces/v2k
+
 /workspaces/v2k/native/extern/vcpkg/vcpkg install protobuf
 sudo ln -sf                                                                               \
   /workspaces/v2k/native/extern/vcpkg/packages/protobuf_x64-linux/tools/protobuf/protoc-* \
   /usr/local/bin/protoc
 
-curl -LO https://github.com/redpanda-data/redpanda/releases/latest/download/rpk-linux-amd64.zip
-sudo unzip rpk-linux-amd64.zip -d /usr/local/bin
-rm rpk-linux-amd64.zip
-task rpk:profile:create
-
-touch $HOME/.bash_profile
 cat .devcontainer/.profile >> $HOME/.bash_profile
 
